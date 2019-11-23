874d06ef0e9a953ce36273d4400210b6ecc2561b
tuyigi <tuyigilbert97@gmail.com>
2019-11-23 17:38:21 +0200

    Done with class of customer,  account and also with deposit

---
+++ BankApp/Customer.cs
@@ -0,0 +1,40 @@
+namespace BankApp
+{
+    public class Customer
+    {
+        private string firstName;
+        private string lastName;
+        private string gender;
+
+        public void setFirstName(double firstName)
+        {
+            this.firstName = this.firstName;
+        }
+
+        public void setLastName(double lastName)
+        {
+            this.lastName = this.lastName;
+        }
+
+        public void setGender(double gender)
+        {
+            this.gender = this.gender;
+        }
+
+        public string getFirstName()
+        {
+            return firstName;
+        }
+        
+        public string getLasttName()
+        {
+            return lastName;
+        }
+
+        public string getGender()
+        {
+            return gender;
+        }
+
+    }
+}
